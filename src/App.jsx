6453f1f8702ed477d7143806eb0a15c8b396be3a
muhammad umair shiekh <umairshiekh01@gmail.com>
2023-10-05 19:38:26 +0500

    assignment

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import Facebook from './Components/Facebook';
+
+import {  useState ,useEffect } from 'react';
+
+function App() {
+const date = new Date
+const newDate = `${date.getHours() + "h" + " ago" }`
+  return (
+    <div className="App">
+      <header className="App-header">           
+      
+      <Facebook userName="Muhammad Umair"  tittle="Today had lunch at Kolachi" profilePic={["https://i.pinimg.com/736x/ed/7f/6b/ed7f6beb78b7fdd9193856fed36931e1.jpg" , "https://images.unsplash.com/photo-1593696954577-ab3d39317b97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60" , "https://images.unsplash.com/photo-1499209974431-9dddcece7f88?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" , "https://images.unsplash.com/photo-1578645635737-6a88e706e0f1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80" , "https://images.unsplash.com/photo-1526779259212-939e64788e3c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60" ,"https://i.pinimg.com/736x/ed/7f/6b/ed7f6beb78b7fdd9193856fed36931e1.jpg"]}
+       date={newDate} 
+      />      
+      
+      
+      
+      
+      
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Facebook.js
@@ -0,0 +1,42 @@
+import FbImageLibrary from 'react-fb-image-grid'
+
+
+function Facebook(props) {
+
+    const { tittle, userName, profilePic, date } = props
+    return (
+        <div className="data" >
+            <div className="main" >
+
+                <div className="profile"  >
+                    <img
+                        className="avatar"
+                        src="https://images.unsplash.com/profile-fb-1696100282-96b1e7fed8de.jpg?dpr=2&auto=format&fit=crop&w=150&h=150&q=60&crop=faces&bg=fff"
+                        alt="Post"
+                        width={60}
+                        height={60}
+                    />
+                    <p>{userName}</p>
+                    <p className="date" >{date}</p>
+                </div>
+                <div className="post" >
+                    <p> {tittle}</p>
+                    {/* <p>{date}</p> */}
+                </div>
+                <div className="images-post" >
+                
+                <FbImageLibrary images={profilePic}/>
+
+                    {/* <img  src={profilePic} /> */}
+                </div>
+                <div>
+                👍 Like
+            </div>
+            </div>
+            
+        </div>
+
+    )
+}
+
+export default Facebook
